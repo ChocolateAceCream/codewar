9b46ce36129372846cb8ca9085521c0d93d11fe2
ChocolateAceCream <nuodi@hotmail.com>
2017-04-14 00:28:02 -0400

    gg

--- test.rb
+++ test.rb
@@ -1,4 +1,23 @@
-markers = ["$","#"]
- p "a #b\nc$\nd $e f g".gsub(/\s+[#{markers.join}].*/, "")
+def next_bigger(n)
+  p n
+  n = n.to_s.chars.map(&:to_i)
+  return -1 if n.length == 1
+  (1..n.length).each do |i|
+    (i+1..n.length).each do |j|
+        (1..j).each do |k|
 
-p "abcedasdf".split-"cefagsdf".split
+        end
+    if n[-i] == 0
+      n[-i],n[-j] = n[-j], n[-i]
+    elsif n[-i]==n[-j]
+        i+=1
+      elsif n[-i]>n[-j]
+
+        n[-i],n[-j] = n[-j], n[-i]
+        return n.join.to_i
+      end
+    end
+  end
+  return -1
+  #your code hereretu
+end
